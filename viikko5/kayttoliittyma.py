621bc129a13beae13ecad06526963027cbc7b50f
gitjuli94 <149376454+gitjuli94@users.noreply.github.com>
2024-12-02 16:46:16 +0100

    Update kayttoliittyma.py

--- viikko5/kayttoliittyma.py
+++ viikko5/kayttoliittyma.py
@@ -9,10 +9,17 @@ class Komento(Enum):
     KUMOA = 4
 
 
-class Kayttoliittyma:
+class Kayttoliittyma: #komentotehdas
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+
+        self._komennot = {
+                Komento.SUMMA: plus(sovelluslogiikka, self._lue_syote),
+                Komento.EROTUS: miinus(sovelluslogiikka, self._lue_syote),
+                Komento.NOLLAUS: nollaa(sovelluslogiikka, self._lue_syote),
+                Komento.KUMOA: kumoa(sovelluslogiikka) 
+            }
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -54,23 +61,12 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _luo_syote(self):
+        return self._syote_kentta.get()
+        
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovelluslogiikka.arvo() == 0:
